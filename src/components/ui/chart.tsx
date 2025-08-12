37ab5bc23116da07bf1652652406d600d8c2c812
joshuaddf <jkdodofoli@gmail.com>
2025-08-12 02:58:58 +0000

    changed THEMES to themes

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -113,7 +113,7 @@ export type ChartConfig = {
     icon?: React.ComponentType
   } & (
     | { color?: string; theme?: never }
-    | { color?: never; theme: Record<keyof typeof THEMES, string> }
+    | { color?: never; theme: Record<keyof typeof themes, string> }
   )
 }
 
@@ -180,7 +180,7 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   return (
     <style
       dangerouslySetInnerHTML={{
-        __html: Object.entries(THEMES)
+        __html: Object.entries(themes)
           .map(
             ([theme, prefix]) => `
 ${prefix} [data-chart=${id}] {
